9670e8ae2f4fe05f9c25c77628c17fd336022b9a
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-10-20 10:32:34 +0300

    created project

---
+++ Rover/Rover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 10/20/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
