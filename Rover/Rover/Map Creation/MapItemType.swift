9ffabc951d53cbb0d980e26d3d01250ef019c52e
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-10-20 12:44:48 +0300

    added mapItem and mapItem type

---
+++ Rover/Rover/Map Creation/MapItemType.swift
@@ -0,0 +1,16 @@
+//
+//  MapItemType.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 10/20/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import Foundation
+
+enum MapItemType {
+    case ground
+    case quicksand
+    case pit
+    case hill
+}
