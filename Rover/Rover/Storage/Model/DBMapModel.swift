54a4d65c8ec3071a7074260efb82c965b52de570
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-10-23 14:48:10 +0300

    added realm, storage manager and DBMapModel

---
+++ Rover/Rover/Storage/Model/DBMapModel.swift
@@ -0,0 +1,35 @@
+//
+//  DBMapModel.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 10/23/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import RealmSwift
+
+class DBMapModel: Object {
+    
+    @objc dynamic var name = ""
+    
+    let mapItems = List<DBMapItem>()
+    
+    convenience init(name: String) {
+        
+        self.init()
+        
+        self.name = name
+    }
+}
+
+
+class DBMapItem: Object {
+    
+    @objc dynamic var surfacetype = ""
+    @objc dynamic var sectionIndex = 0
+    @objc dynamic var indexInSection = 0
+    
+    var indexPath: IndexPath {
+        return IndexPath(item: indexInSection, section: sectionIndex)
+    }
+}
---
+++ Rover/Rover/Storage/StorageManager.swift
@@ -0,0 +1,16 @@
+//
+//  StorageManager.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 10/23/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    
+}
