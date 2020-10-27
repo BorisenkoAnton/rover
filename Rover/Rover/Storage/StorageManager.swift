8b68a6ac3393d01529a5ad00b04b6103c9603866
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-10-27 16:41:06 +0300

    added saving and deleting from firebase, changing map name

--- Rover/Rover/Storage/StorageManager.swift
+++ Rover/Rover/Storage/StorageManager.swift
@@ -7,8 +7,11 @@
 //
 
 import RealmSwift
+import Firebase
 
 let realm = try! Realm()
+
+let firestore = Firestore.firestore()
 
 class StorageManager {
     
@@ -17,13 +20,47 @@ class StorageManager {
         try! realm.write {
             realm.add(map)
         }
+        
+        if let mapAsDictionary = map.asDictionary {
+            firestore.collection("maps").document(map.name).setData(mapAsDictionary)
+        }
     }
     
     
     static func deleteMap(_ map:DBMapModel) {
         
+        firestore.collection("maps").document(map.name).delete()
+        
         try! realm.write {
             realm.delete(map)
         }
+    }
+    
+    
+    static func changeMapname(_ map: DBMapModel, newName: String) {
+        
+        let mapName = map.name
+        
+        firestore.collection("maps").document(mapName).updateData(["name": newName])
+        
+        firestore.collection("maps").document(mapName).getDocument { (document, error) in
+            
+            if let data = document?.data() {
+                print("sfdsfsdf")
+                firestore.collection("maps").document(newName).setData(data)
+            }
+            
+            firestore.collection("maps").document(mapName).delete()
+        }
+        
+        try! realm.write {
+            map.name = newName
+        }
+    }
+    
+    
+    static func synchronize() {
+        
+        
     }
 }
