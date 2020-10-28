9d83b31d4ca6a4388c7aa224ad6f54f2b64e4bca
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-10-28 17:01:04 +0300

    added timestamp and decoding from dictionary

--- Rover/Rover/Storage/Model/DBMapModel.swift
+++ Rover/Rover/Storage/Model/DBMapModel.swift
@@ -11,18 +11,33 @@ import RealmSwift
 class DBMapModel: Object, Codable {
     
     @objc dynamic var name = ""
+    @objc dynamic var timestamp: Int = Int(Date().timeIntervalSince1970)
     
     var mapItems = List<DBMapItem>()
     var asDictionary: [String: Any]? {
       guard let data = try? JSONEncoder().encode(self) else { return nil }
+        
       return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
     }
     
-    convenience init(name: String, mapItems: List<DBMapItem>) {
+    
+    convenience init(name: String, timestamp: Int, mapItems: List<DBMapItem>) {
         
         self.init()
         
         self.name = name
+        self.timestamp = timestamp
+        self.mapItems = mapItems
+    }
+}
+
+
+extension Decodable {
+    
+    init(from: Any) throws {
+      let data = try JSONSerialization.data(withJSONObject: from, options: .prettyPrinted)
+      let decoder = JSONDecoder()
+      self = try decoder.decode(Self.self, from: data)
     }
 }
 
