723a8cb5fbce0156a94718c0dbc6aafafbb553dc
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-10-30 17:08:30 +0300

    added simulation view (without any configuration and functions)

---
+++ Rover/Rover/Simulation/View/SimulationView.swift
@@ -0,0 +1,21 @@
+//
+//  SimulationView.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 10/30/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import UIKit
+
+class SimulationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Rover/Rover/Simulation/View/SimulationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SimulationViewController.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 10/30/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import Foundation
