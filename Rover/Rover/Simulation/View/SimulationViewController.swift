3c04a8994149bc0ed2175903034156277be7a3de
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-11-02 17:47:00 +0300

    added configuring view

--- Rover/Rover/Simulation/View/SimulationViewController.swift
+++ Rover/Rover/Simulation/View/SimulationViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 import GLKit
 import OpenGLES
 
-class SimulationViewController: GLKViewController {
+
+class SimulationViewController: GLKViewController, SimulationViewControllerDelegate {
+    
+    var simulationPresenterDelegate: SimulationPresenterDelegate?
+    
     
     override func loadView() {
         
+        setupGL()
+        self.simulationPresenterDelegate?.setMapToView()
+    }
+
+    
+    func setSimulationPresenterDelegate(delegate: SimulationPresenterDelegate) {
+        
+        self.simulationPresenterDelegate = delegate
+    }
+    
+
+    private func setupGL() {
+        
         self.view = SimulationView()
+        
+        if let eaglContext = EAGLContext(api: .openGLES3) {
+            
+            (self.view as! SimulationView).context = eaglContext
+            (self.view as! SimulationView).ciContext = CIContext(eaglContext: eaglContext)
+        }
+    }
+    
+    
+    func setMap(map: [MapSector]) {
+        
+        (self.view as! SimulationView).map = map
+        
+        self.view.setNeedsDisplay()
     }
 }
