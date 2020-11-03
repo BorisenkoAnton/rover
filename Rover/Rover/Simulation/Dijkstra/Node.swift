2e5e3dd04ce9e9e84b83059834a0b6b71dcda500
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-11-03 14:39:33 +0300

    added classes to implement dijkstra's algorithm

---
+++ Rover/Rover/Simulation/Dijkstra/Node.swift
@@ -0,0 +1,15 @@
+//
+//  Node.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 11/3/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import Foundation
+
+class Node {
+    
+    var visited = false
+    var connections: [Connection] = []
+}
