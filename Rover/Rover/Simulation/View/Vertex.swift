d93605f691b36750a4dea88e27b2e7e89c6410c9
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-11-04 17:24:30 +0300

    added struct representing vertex in OpenGL

---
+++ Rover/Rover/Simulation/View/Vertex.swift
@@ -0,0 +1,19 @@
+//
+//  Vertex.swift
+//  Rover
+//
+//  Created by Anton Borisenko on 11/4/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import GLKit
+
+struct Vertex {
+    var x: GLfloat
+    var y: GLfloat
+    var z: GLfloat
+    var r: GLfloat
+    var g: GLfloat
+    var b: GLfloat
+    var a: GLfloat
+}
