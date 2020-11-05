65eda5140869e34cc9dfef6030d1c0134f4330d7
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-11-05 13:07:26 +0300

    removed unused settings

--- Rover/Rover/Simulation/View/Extensions/SimulationViewController+GLKViewControllerDelegate.swift
+++ Rover/Rover/Simulation/View/Extensions/SimulationViewController+GLKViewControllerDelegate.swift
@@ -14,17 +14,5 @@ extension SimulationViewController: GLKViewControllerDelegate {
 
     func glkViewControllerUpdate(_ controller: GLKViewController) {
 
-        // Calculating the aspect ratio of the GLKView
-        let aspect = fabsf(Float(view.bounds.size.width) / Float(view.bounds.size.height))
-        
-        // Creating a perspective matrix
-        let projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0), aspect, 4.0, 10.0)
-        
-        effect.transform.projectionMatrix = projectionMatrix
-        
-        // Creating a matrix that translates six units backwards
-        let modelViewMatrix = GLKMatrix4MakeTranslation(0.0, 0.0, -6.0)
-
-        effect.transform.modelviewMatrix = modelViewMatrix
     }
 }
